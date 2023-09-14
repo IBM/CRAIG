e1f81dcacb8cf46ae48eab600cfdcfe5dee7d5a3
jvallexm <jvallexm@gmail.com>
2023-09-14 13:03:05 -0400

    hotfix: cdktf

--- express-routes/routes.js
+++ express-routes/routes.js
@@ -1,12 +1,8 @@
 const { Router } = require("express");
 const axios = require("axios");
 const router = Router();
-const jsutil = require("util"); // Utils to run child process
-const exec = jsutil.promisify(require("child_process").exec); // Exec from child process
 const apiController = require("../express-controllers/controller");
-const cdktfController = require("../express-controllers/cdktf");
 const controller = new apiController(axios);
-const cdktf = new cdktfController(exec);
 
 // vsi
 router.get("/vsi/:region/instanceProfiles", controller.vsiInstanceProfiles);
