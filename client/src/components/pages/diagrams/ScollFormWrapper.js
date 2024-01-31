e7c470da5bbd1d184b31fb9d2ebfbb9eb3825ffd
Pseusco <lfranke@vt.edu>
2024-01-31 13:11:15 -0500

    Issue 1343: Removing icse-react-asset imports from EdgeNetworkingForm (#1437)
    * edge-networking state wip
    
    Signed-off-by: Lucas-Franke <Lucas.Franke@ibm.com>
    
    * edit edge networking form to use CraigToggleForm and DynamicFormSelect
    
    Signed-off-by: Lucas-Franke <Lucas.Franke@ibm.com>
    
    * remove unused import
    
    Signed-off-by: Lucas-Franke <Lucas.Franke@ibm.com>
    
    * remove icse-react-asset import
    
    Signed-off-by: Lucas-Franke <Lucas.Franke@ibm.com>
    
    * use statelessformwrapper and disablesave func
    
    Signed-off-by: Lucas-Franke <Lucas.Franke@ibm.com>

--- client/src/components/pages/diagrams/ScollFormWrapper.js
+++ client/src/components/pages/diagrams/ScollFormWrapper.js
@@ -1,6 +1,6 @@
 import "./diagrams.css";
 
-export const ScrollFormWrapper = props => {
+export const ScrollFormWrapper = (props) => {
   return (
     <div className="scrollFormWrapper">
       <div className="overflow-auto">{props.children}</div>
